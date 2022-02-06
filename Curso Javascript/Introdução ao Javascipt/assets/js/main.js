62c11b019e63fbd9f25d8ec92d179da226e71f88
SamuelGDN <samuelgonzagasg1@gmail.com>
2022-02-05 22:38:02 -0300

    Resolução Desefio

--- Curso Javascript/Introdução ao Javascipt/assets/js/main.js
+++ Curso Javascript/Introdução ao Javascipt/assets/js/main.js
@@ -2,26 +2,36 @@ var CurrentNumberwrapper = document.getElementById('currentNumber')
 var btn1 = document.getElementById('btn-')
 var btn2 = document.getElementById('btn+')
 var Currentnumber = 0;
-var count = 0;
 
-function disable() {
-    btn2.classList.add('disabled')
+function disable(btn) {
+    btn.classList.add('disabled')
+}
+
+function removeDisable(btn) {
+    btn.classList.remove('disabled')
 }
 
 function increment(){
+    removeDisable(btn1);
     if(Currentnumber<10){
         Currentnumber = Currentnumber + 1;
-    CurrentNumberwrapper.innerHTML = Currentnumber;
+        CurrentNumberwrapper.innerHTML = Currentnumber;
     } else{
-        disable();
+        disable(btn2);
     }
     
 }
 
 function decrement(){
-    Currentnumber = Currentnumber - 1;
-    CurrentNumberwrapper.innerHTML = Currentnumber;
+    removeDisable(btn2);
+    if(Currentnumber>-10){
+        Currentnumber = Currentnumber - 1;
+        CurrentNumberwrapper.innerHTML = Currentnumber;
+    } else{
+        disable(btn1);
+    }
 }
 
 btn1.addEventListener('click',decrement);
 btn2.addEventListener('click',increment);
+
